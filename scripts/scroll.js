dd37f3cb7d1d2af0898e3193bb62d1b27d38ffc6
Melanie Mohn <melaniemohn@yahoo.com>
2018-05-24 22:07:26 -0400

    Sloppy commit to add parallax scrolling and extra images.

---
+++ scripts/scroll.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.intro-section').parallax({imageSrc: '../public/images/title-image.jpg'});
+	// $('#our-wedding').parallax({
+	// 	imageSrc: '../public/images/toms-town-crop.jpg',
+	// 	position: 'right bottom',
+	// 	naturalWidth: 40
+	// });
+});

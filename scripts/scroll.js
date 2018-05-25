33551de1299faa333197d4fb8ca10f9fd1fd9ad5
Melanie Mohn <melaniemohn@yahoo.com>
2018-05-24 23:45:58 -0400

    Separate details page and update navigation

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
 	$('.intro-section').parallax({imageSrc: '../public/images/title-image.jpg'});
-	// $('#our-wedding').parallax({
-	// 	imageSrc: '../public/images/toms-town-crop.jpg',
-	// 	position: 'right bottom',
-	// 	naturalWidth: 40
-	// });
 });
